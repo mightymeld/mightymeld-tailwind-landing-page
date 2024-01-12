37405d452235d7cacd6f64f306ad25744022ce3d
Diseyi <filomiinaa@gmail.com>
2024-01-12 22:04:44 +0100

    init MM and add prefabs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const conditionalPlugins = [];
+
+if (process.env.MIGHTYMELD) {
+  conditionalPlugins.push('@mightymeld/runtime/babel-plugin-mightymeld');
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: [...conditionalPlugins]
+      }
+    })
+  ]
+});
