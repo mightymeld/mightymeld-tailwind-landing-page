51c128a929fa13bcdb5d47e29d1600b7614def85
Diseyi <filomiinaa@gmail.com>
2024-06-14 13:09:59 +0100

    add new file

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react';
 
 const conditionalPlugins = [];
 
-if (process.env.MIGHTYMELD) {
-  conditionalPlugins.push('@mightymeld/runtime/babel-plugin-mightymeld');
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
