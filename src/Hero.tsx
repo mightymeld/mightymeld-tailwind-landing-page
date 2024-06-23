db9b03fe6158c2580ff99dea58b1a6483b6668bb
Diseyi <filomiinaa@gmail.com>
2024-06-23 13:13:40 +0100

    update

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
                     <p className="mt-5 sm:mt-10 lg:w-10/12 text-slate-500 font-normal text-center text-sm sm:text-lg">The seamless component building blocks for effortlessly crafting your perfect web application </p>
                 </div>
                 <div className="flex justify-center items-center">
-                    <button className="  bg-sky-700 transition duration-150 ease-in-out hover:bg-sky-600 lg:text-xl lg:font-bold  rounded text-white px-4 sm:px-10 py-2 sm:py-4 text-sm">Get Started Now</button>
+                    <button className="  bg-sky-700 transition duration-150 ease-in-out hover:bg-sky-600 lg:text-xl lg:font-bold  rounded text-white px-4 sm:px-10 py-2 sm:py-4 text-sm">Get Started</button>
                     <button className="ml-4   bg-transparent transition duration-150 ease-in-out hover:border-sky-600 lg:text-xl lg:font-bold  hover:text-sky-600 rounded border border-sky-700 text-sky-700 px-4 sm:px-10 py-2 sm:py-4 text-sm">Book Demo</button>
                 </div>
             </div>
